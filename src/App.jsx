df94fc077abe83961b46d082eee9ba1f2b14a357
thedevaman <devaman2508@gmail.com>
2025-10-10 08:59:39 +0530

    qr-code-generated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import { Button, QRCode } from "antd";
-import { Download } from "lucide-react";
-import React, { useRef } from "react";
+import { Button, Form, Input, Modal, QRCode } from "antd";
+import { Download, RefreshCcw } from "lucide-react";
+import React, { useRef, useState } from "react";
 import '@ant-design/v5-patch-for-react-19';
+import { useForm } from "antd/es/form/Form";
 
 
 const App = () => {
+
+  const [form] = useForm()
+
+  const [icon ,setIcon] = useState(null)
+  const [open,SetOpen] = useState(false)
+  const [qr,SetQr] = useState({
+    value:'https://www.youtube.com/',
+    icon:'',
+    bgcolor:'white',
+    color:'black'
+  })
+
   const divRef = useRef(null)
   const downloadNow = ()=>{
    const div = divRef.current
@@ -17,25 +30,113 @@ const App = () => {
    a.remove()
   }
 
+  const generateQr = (values) =>{
+    
+    values.background_color = values.background_color || "white"
+    values.color = values.color || "black"
+    values.icon = icon
+    SetQr((prev)=>({
+      ...prev,
+      ...values
+
+    }))
+    SetOpen(false)
+  }
+
+  const chooseFile = (e)=>{
+    const file = e.target.files[0]
+    const url = URL.createObjectURL(file)
+    setIcon(url)
+    }
+
+    const handleClose = () =>{
+      SetOpen(false)
+      form.resetFields()
+      setIcon('')
+    }
+
+
   return(
     <div className="bg-gray-100 h-screen py-12 flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold mb-16">QR Code Generator</h1>
       <div ref={divRef} className="mb-12 rounded-xl p-4 bg-white shadow-lg w-fit hover:scale-115 transition-transform duration-300 hover:shadow-2xl">
-       <QRCode value="https://www.youtube.com/"
+       <QRCode value={qr.value}
        size = {300}
-       icon="https://api.dicebear.com/7.x/adventurer/svg?seed=tFvXxODI1AzZ03ymPwBhJ"
-       bgColor="black"
-       color="white"
+       icon={qr.icon}
+       bgColor={qr.bgcolor}
+       color={qr.color}
        />
       </div>
-      <div>
+      <div className="flex gap-2">
         <Button size="large" 
         type="primary" 
-        className="!bg-gradient-to-br !from-violet-600 !via-blue-500 !to-indigo-600 "
+        className="!bg-gradient-to-r !from-violet-600 !via-blue-500 !to-indigo-600 !border-none hover:!scale-105 "
         icon={<Download className="w-4 h-4" />}
         onClick={downloadNow}
         >Download Now</Button>
+
+         <Button size="large" 
+        type="primary" 
+        className="!bg-gradient-to-r !from-green-600 !via-emerald-600 !to-cyan-600 !border-none hover:!scale-105 "
+        icon={<RefreshCcw className="w-4 h-4" />}
+        onClick={()=>SetOpen(true)}
+        >Generate New QR</Button>
       </div>
+
+      <Modal open={open} footer={null} onCancel={handleClose}>
+        <h1 className="text-lg font-medium">Generate Qr</h1>
+        <Form  onFinish={generateQr} form={form}>
+          <Form.Item 
+          label="URL"
+          rules={[{required:true,type:"url"}]}
+          name="url"
+          >
+            <Input 
+            size="large"
+            placeholder="https://domainname.com"
+            />
+          </Form.Item>
+
+           <Form.Item 
+          label="BG Color"
+          name="background_color"
+          >
+            <Input 
+            size="large"
+            type="color"
+            />
+          </Form.Item>
+
+           <Form.Item 
+          label="Color"
+          name="color"
+          >
+            <Input 
+            size="large"
+            type="color"
+            />
+          </Form.Item>
+
+           <Form.Item 
+          label="Logo"
+          name="logo"
+          >
+            <Input 
+            size="large"
+            type="file"
+            accept="image/*"
+            onChange={chooseFile}
+            />
+          </Form.Item>
+
+          <Form.Item>
+            <Button size="large" type="primary" htmlType="submit">
+              Generate
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
+      
     </div>
   )
 
